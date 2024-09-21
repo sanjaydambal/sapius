13437fa32ce39e6b62dc37d6f69d235a3366f761
SvdNeo <sanjay.d@neoito.com>
2024-09-21 18:57:43 +0530

    created the team members with sliding image"

--- src/App.js
+++ src/App.js
@@ -2,16 +2,12 @@ import logo from './logo.svg';
 import './App.css';
 import Header from './components/Header';
 import Story from './components/Story';
-import "slick-carousel/slick/slick.css"; 
-import "slick-carousel/slick/slick-theme.css";
-import Portfolio from './components/Portfolio';
 
 function App() {
   return (
     <div className="App">
    <Header/>
    <Story/>
-   <Portfolio/>
     </div>
   );
 }
