eddcb61fe562d52e9b037a109ff82c80076880fc
SvdNeo <sanjay.d@neoito.com>
2024-09-21 19:07:18 +0530

    updated app

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import Header from './components/Header';
 import Story from './components/Story';
+import Portfolio from './components/Portfolio';
 
 function App() {
   return (
     <div className="App">
    <Header/>
    <Story/>
+   <Portfolio/>
     </div>
   );
 }
