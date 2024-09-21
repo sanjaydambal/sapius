25a8cd7a0fc0b5fbc090d5c1547a4b4e206eb510
SvdNeo <sanjay.d@neoito.com>
2024-09-21 19:12:43 +0530

    updated the css of app component

--- src/App.js
+++ src/App.js
@@ -2,12 +2,16 @@ import logo from './logo.svg';
 import './App.css';
 import Header from './components/Header';
 import Story from './components/Story';
+import Portfolio from './components/Portfolio';
+import "slick-carousel/slick/slick.css"; 
+import "slick-carousel/slick/slick-theme.css";
 
 function App() {
   return (
     <div className="App">
    <Header/>
    <Story/>
+   <Portfolio/>
     </div>
   );
 }
